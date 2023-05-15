06c3d7480ec446c07245b47b84595de36572d6cb
juanca1013 <jmarmol1@my.centennialcollege.ca>
2023-05-15 14:29:50 -0400

    Set up project

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import { Inter } from 'next/font/google'
+import { Banner } from '../../components/Banner/Banner'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main className={`${inter.className}`}>
+        <Banner/>
+    </main>
+  )
+}
