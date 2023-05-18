9ef557ea1d08fdf717e0684c6e278294116542ab
juanca1013 <jmarmol1@my.centennialcollege.ca>
2023-05-18 11:09:18 -0400

    Add resume to about me

--- components/AboutMe/AboutMeSection.jsx
+++ components/AboutMe/AboutMeSection.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
 import myPhoto from '../../public/myPhoto.jpeg'
+import Link from 'next/link'
 import { CardList } from './CardList'
 import {MdWeb} from 'react-icons/md'
-import {AiFillMobile} from 'react-icons/ai'
+import {AiFillMobile,AiFillFile} from 'react-icons/ai'
 import {BsFillClipboardDataFill} from 'react-icons/bs'
 import {SiSpeedtest} from 'react-icons/si'
 import {FiFigma} from 'react-icons/fi'
@@ -41,8 +42,14 @@ export const AboutMeSection = () => {
                 />
             </div>
             <div className='flex-row w-[90%] mx-[5%] text-center lg:text-left lg:ml-5  lg:w-[40%] lg:mr-[10%]  lg:justify-start'>
-                <h3 className='text-[#041E40] text-4xl mb-2'>My, Myself & I</h3>
-                <p>I am a passionate software engineering technology student at Centennial College at Toronto, Canada. With a strong enthusiasm for coding, I constantly strive to expand my knowledge and skills in the field. I am dedicated to creating efficient and innovative solutions, leveraging my expertise in various programming languages and technologies. My portfolio showcases a diverse range of projects that demonstrate my ability to tackle real-world challenges and deliver high-quality results. With a keen eye for detail and a strong commitment to excellence, I am eager to contribute my skills to the dynamic world of software development.</p>
+                <h2 className='text-[#041E40] text-4xl mb-2'>My, Myself & I</h2>
+                <p>I am a passionate software engineering technology student at Centennial College at Toronto, Canada. With a strong enthusiasm for coding, I constantly strive to expand my knowledge and skills in the field. I am dedicated to creating efficient and innovative solutions, leveraging my expertise in various programming languages and technologies. My portfolio showcases a diverse range of projects that demonstrate my ability to tackle real-world challenges and deliver high-quality results.</p>
+                <Link href="https://drive.google.com/file/d/1uzW_1nCiBCmeA2IOUXkbgbBvysDV6kHJ/view?usp=sharing" target='_blank'>
+                  <div className='flex items-center text-center justify-center mt-2 text-[#736626]'>
+                    <p>My resume</p>
+                    <AiFillFile size={20}/>
+                  </div>
+                </Link>
             </div>
         </div>
         <div className='flex flex-wrap justify-center align-middle items-center mt-10'>
