63fa3d6f3720a6b4a53675e013fdb82b4ceca225
juanca1013 <jmarmol1@my.centennialcollege.ca>
2023-05-20 14:39:22 -0400

    Add head, contact and footer component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,31 @@
 import { Inter } from 'next/font/google'
 import { AboutMeSection } from '../../components/AboutMe/AboutMeSection'
 import { Banner } from '../../components/Banner/Banner'
+import { ContactSection } from '../../components/Contact/ContactSection'
+import { Footer } from '../../components/Footer/Footer'
 import { NavBar } from '../../components/NavBar/NavBar'
 import { ProjectsSection } from '../../components/Projects/ProjectsSection'
 import { SkillsSection } from '../../components/Skills/SkillsSection'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    <>
+    <Head>
+        <title>Camilo's Portfolio</title>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
     <main className={`${inter.className}`}>
         <Banner/>
         <NavBar/>
         <AboutMeSection />
         <SkillsSection />
         <ProjectsSection />
+        <ContactSection/>
+        <Footer/>
     </main>
+    </>
   )
 }
