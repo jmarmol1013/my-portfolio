cdc0b987d1d77b3c327acecb8388e4c39ccd0cb9
juanca1013 <jmarmol1@my.centennialcollege.ca>
2023-05-20 14:50:27 -0400

    Atom changes

--- components/Contact/Atom.jsx
+++ components/Contact/Atom.jsx
@@ -3,65 +3,65 @@ import React from 'react'
 export const Atom = () => {
   return (
     <div>
-		<div class="pl">
-        <svg class="pl__rings" viewBox="0 0 128 128" width="128px" height="128px">
-		<g fill="none" stroke-linecap="round" stroke-width="4">
-			<g class="pl__ring" transform="rotate(0)">
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0.3)" />
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0.5)" stroke-dasharray="50 240" />
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsl(223,90%,50%)" stroke-dasharray="25 265" />
+		<div className="pl">
+        <svg className="pl__rings" viewBox="0 0 128 128" width="128px" height="128px">
+		<g fill="none" strokeLinecap="round" strokeWidth="4">
+			<g className="pl__ring" transform="rotate(0)">
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0.3)" />
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0.5)" strokeDasharray="50 240" />
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsl(223,90%,50%)" strokeDasharray="25 265" />
 			</g>
-			<g class="pl__ring" transform="rotate(0)">
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0)" />
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0.5)" stroke-dasharray="50 240" />
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsl(223,90%,50%)" stroke-dasharray="25 265" />
+			<g className="pl__ring" transform="rotate(0)">
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0)" />
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0.5)" strokeDasharray="50 240" />
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsl(223,90%,50%)" strokeDasharray="25 265" />
 			</g>
-			<g class="pl__ring" transform="rotate(0)">
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0)" />
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0.5)" stroke-dasharray="50 240" />
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsl(223,90%,50%)" stroke-dasharray="25 265" />
+			<g className="pl__ring" transform="rotate(0)">
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0)" />
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0.5)" strokeDasharray="50 240" />
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsl(223,90%,50%)" strokeDasharray="25 265" />
 			</g>
-			<g class="pl__ring" transform="rotate(0)">
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0)" />
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0.5)" stroke-dasharray="50 240" />
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsl(223,90%,50%)" stroke-dasharray="25 265" />
+			<g className="pl__ring" transform="rotate(0)">
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0)" />
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0.5)" strokeDasharray="50 240" />
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsl(223,90%,50%)" strokeDasharray="25 265" />
 			</g>
-			<g class="pl__ring" transform="rotate(180)">
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0.3)" />
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0.5)" stroke-dasharray="50 240" />
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsl(223,90%,50%)" stroke-dasharray="25 265" />
+			<g className="pl__ring" transform="rotate(180)">
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0.3)" />
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0.5)" strokeDasharray="50 240" />
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsl(223,90%,50%)" strokeDasharray="25 265" />
 			</g>
-			<g class="pl__ring" transform="rotate(180)">
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0)" />
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0.5)" stroke-dasharray="50 240" />
-				<ellipse class="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsl(223,90%,50%)" stroke-dasharray="25 265" />
+			<g className="pl__ring" transform="rotate(180)">
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0)" />
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsla(223,90%,50%,0.5)" strokeDasharray="50 240" />
+				<ellipse className="pl__orbit" cx="64" cy="64" rx="60" ry="30" stroke="hsl(223,90%,50%)" strokeDasharray="25 265" />
 			</g>
-			<g class="pl__ring" transform="rotate(0)">
-				<ellipse class="pl__electron" cx="64" cy="64" rx="60" ry="30" stroke="hsl(0,0%,100%)" stroke-dasharray="1 289" stroke-width="8" />
-				<ellipse class="pl__electron" cx="64" cy="64" rx="60" ry="30" stroke="hsl(0,0%,100%)" stroke-dasharray="1 289" stroke-width="8" />
+			<g className="pl__ring" transform="rotate(0)">
+				<ellipse className="pl__electron" cx="64" cy="64" rx="60" ry="30" stroke="hsl(0,0%,100%)" strokeDasharray="1 289" strokeWidth="8" />
+				<ellipse className="pl__electron" cx="64" cy="64" rx="60" ry="30" stroke="hsl(0,0%,100%)" strokeDasharray="1 289" strokeWidth="8" />
 			</g>
-			<g class="pl__ring" transform="rotate(180)">
-				<ellipse class="pl__electron" cx="64" cy="64" rx="60" ry="30" stroke="hsl(0,0%,100%)" stroke-dasharray="1 289" stroke-width="8" />
-				<ellipse class="pl__electron" cx="64" cy="64" rx="60" ry="30" stroke="hsl(0,0%,100%)" stroke-dasharray="1 289" stroke-width="8" />
-				<ellipse class="pl__electron" cx="64" cy="64" rx="60" ry="30" stroke="hsl(0,0%,100%)" stroke-dasharray="1 289" stroke-width="8" />
-				<ellipse class="pl__electron" cx="64" cy="64" rx="60" ry="30" stroke="hsl(0,0%,100%)" stroke-dasharray="1 289" stroke-width="8" />	
+			<g className="pl__ring" transform="rotate(180)">
+				<ellipse className="pl__electron" cx="64" cy="64" rx="60" ry="30" stroke="hsl(0,0%,100%)" strokeDasharray="1 289" strokeWidth="8" />
+				<ellipse className="pl__electron" cx="64" cy="64" rx="60" ry="30" stroke="hsl(0,0%,100%)" strokeDasharray="1 289" strokeWidth="8" />
+				<ellipse className="pl__electron" cx="64" cy="64" rx="60" ry="30" stroke="hsl(0,0%,100%)" strokeDasharray="1 289" strokeWidth="8" />
+				<ellipse className="pl__electron" cx="64" cy="64" rx="60" ry="30" stroke="hsl(0,0%,100%)" strokeDasharray="1 289" strokeWidth="8" />	
 			</g>
 		</g>
 	</svg>
-	<div class="pl__nucleus">
-		<div class="pl__nucleus-particle"></div>
-		<div class="pl__nucleus-particle"></div>
-		<div class="pl__nucleus-particle"></div>
-		<div class="pl__nucleus-particle"></div>
-		<div class="pl__nucleus-particle"></div>
-		<div class="pl__nucleus-particle"></div>
-		<div class="pl__nucleus-particle"></div>
-		<div class="pl__nucleus-particle"></div>
-		<div class="pl__nucleus-particle"></div>
-		<div class="pl__nucleus-particle"></div>
-		<div class="pl__nucleus-particle"></div>
-		<div class="pl__nucleus-particle"></div>
-		<div class="pl__nucleus-particle"></div>
+	<div className="pl__nucleus">
+		<div className="pl__nucleus-particle"></div>
+		<div className="pl__nucleus-particle"></div>
+		<div className="pl__nucleus-particle"></div>
+		<div className="pl__nucleus-particle"></div>
+		<div className="pl__nucleus-particle"></div>
+		<div className="pl__nucleus-particle"></div>
+		<div className="pl__nucleus-particle"></div>
+		<div className="pl__nucleus-particle"></div>
+		<div className="pl__nucleus-particle"></div>
+		<div className="pl__nucleus-particle"></div>
+		<div className="pl__nucleus-particle"></div>
+		<div className="pl__nucleus-particle"></div>
+		<div className="pl__nucleus-particle"></div>
 	</div>
     </div>
 	</div>
